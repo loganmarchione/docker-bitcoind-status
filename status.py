84a8c065f585fa59da3a736dc0a209bf72455399
Logan Marchione <logan@loganmarchione.com>
2021-01-19 12:16:57 -0500

    Combine everything into one function, re-do HTML template

--- status.py
+++ status.py
@@ -7,7 +7,6 @@ import os
 import requests
 import socket
 import sys
-from prettytable import PrettyTable
 from flask import Flask, render_template
 
 # Variables
@@ -32,7 +31,7 @@ def refresh_time() -> datetime:
     return r
 
 
-def price_check(c: str) -> int:
+def price_check(c: str) -> float:
     '''
     Takes in currency code, returns Bitcoin price
 
@@ -40,7 +39,7 @@ def price_check(c: str) -> int:
         c (str):    Currency code from Coinbase https://api.coinbase.com/v2/currencies
 
     Returns:
-        int         Bitcoin price in that currency
+        float       Bitcoin price in that currency
     '''
 
     url = "https://api.coinbase.com/v2/prices/BTC-" + c + "/spot"
@@ -85,173 +84,6 @@ def conn_check():
         sys.exit(1)
 
 
-def build_table1() -> str:
-    '''Takes no input, just builds the table and output HTML'''
-
-    # First, check the connection
-    conn_check()
-
-    # RPC calls to get info
-    headers = {'content-type': 'text/plain'}
-    payload1 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getnetworkinfo", "params": []})
-    r1 = requests.post(connection_string, data=payload1, auth=(rpc_user, rpc_pass), headers=headers)
-    j1 = r1.json()
-    r1.close()
-
-    payload2 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "uptime", "params": []})
-    r2 = requests.post(connection_string, data=payload2, auth=(rpc_user, rpc_pass), headers=headers)
-    j2 = r2.json()
-    r2.close()
-
-    payload3 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getmemoryinfo", "params": ["stats"]})
-    r3 = requests.post(connection_string, data=payload3, auth=(rpc_user, rpc_pass), headers=headers)
-    j3 = r3.json()
-    r3.close()
-
-    # Save the responses to variables
-    subversion = str(j1['result']['subversion'])
-    connections = int(j1['result']['connections'])
-    uptime = int(j2['result'])
-    mem_used = int(j3['result']['locked']['used'])
-    mem_free = int(j3['result']['locked']['free'])  # noqa: F841
-    mem_total = int(j3['result']['locked']['total'])
-    mem_perc = round(((mem_used/mem_total) * 100), 2)
-
-    # Here we assemble the table
-    x = PrettyTable()
-    x.field_names = ["Name", "Value"]
-    x.align = "l"
-    x.add_rows(
-        [
-            ["Node address", connection_string],
-            ["Client version", subversion.strip('/')],
-            ["Connections", connections],
-            ["Uptime (days, hour:min:sec)", datetime.timedelta(seconds=uptime)],
-            ["Memory", "Used: " + str(round((mem_used / 1000), 2)) + " MB of  Total: " + str(round((mem_total / 1000), 2)) + " MB (" + str(mem_perc) + "% used)"]
-        ]
-    )
-    html = x.get_html_string(attributes={"class": "table is-bordered is-striped is-hoverable is-narrow"})
-    return html
-
-
-def build_table2() -> str:
-    '''Takes no input, just builds the table and output HTML'''
-
-    # First, check the connection
-    conn_check()
-
-    # RPC calls to get info
-    headers = {'content-type': 'text/plain'}
-    payload1 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getblockchaininfo", "params": []})
-    r1 = requests.post(connection_string, data=payload1, auth=(rpc_user, rpc_pass), headers=headers)
-    j1 = r1.json()
-    r1.close()
-
-    # Save the responses to variables
-    chain = str(j1['result']['chain'])
-    blocks = int(j1['result']['blocks'])
-    initial = str(j1['result']['initialblockdownload'])
-    verificationprogress = float(j1['result']['verificationprogress'])
-    size_on_disk = float(j1['result']['size_on_disk'])
-    pruned = str(j1['result']['pruned'])
-
-    # Here we assemble the table
-    x = PrettyTable()
-    x.field_names = ["Name", "Value"]
-    x.align = "l"
-    x.add_rows(
-        [
-            ["Chain", chain],
-            ["Block number", blocks],
-            ["Initial block download?", initial],
-            ["Verification", verificationprogress],
-            ["Size on disk", str(round((size_on_disk / (1000**3)), 2)) + " GB"],
-            ["Is pruned?", pruned]
-        ]
-    )
-    html = x.get_html_string(attributes={"class": "table is-bordered is-striped is-hoverable is-narrow"})
-    return html
-
-
-def build_table3() -> str:
-    '''Takes no input, just builds the table and output HTML'''
-
-    # First, check the connection
-    conn_check()
-
-    # RPC calls to get info
-    headers = {'content-type': 'text/plain'}
-    payload1 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getnetworkhashps", "params": [144]})
-    r1 = requests.post(connection_string, data=payload1, auth=(rpc_user, rpc_pass), headers=headers)
-    j1 = r1.json()
-    r1.close()
-
-    payload2 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getnetworkhashps", "params": [-1]})
-    r2 = requests.post(connection_string, data=payload2, auth=(rpc_user, rpc_pass), headers=headers)
-    j2 = r2.json()
-    r2.close()
-
-    payload3 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getmininginfo", "params": []})
-    r3 = requests.post(connection_string, data=payload3, auth=(rpc_user, rpc_pass), headers=headers)
-    j3 = r3.json()
-    r3.close()
-
-    # Save the responses to variables
-    hash_rate = float(j1['result'])
-    hash_rate_last_diff = float(j2['result'])
-    difficulty = float(j3['result']['difficulty'])
-
-    # Here we assemble the table
-    x = PrettyTable()
-    x.field_names = ["Name", "Value"]
-    x.align = "l"
-    x.add_rows(
-        [
-            ["Estimated hash rate (last 144 blocks or ~1 day)", str(round(hash_to_hash(hash_rate), 2)) + " EH/s"],
-            ["Estimated hash rate (since last difficulty adjustment)", str(round(hash_to_hash(hash_rate_last_diff), 2)) + " EH/s"],
-            ["Difficulty", difficulty]
-        ]
-    )
-    html = x.get_html_string(attributes={"class": "table is-bordered is-striped is-hoverable is-narrow"})
-    return html
-
-
-def build_table4() -> str:
-    '''Takes no input, just builds the table and output HTML'''
-
-    # First, check the connection
-    conn_check()
-
-    # RPC calls to get info
-    headers = {'content-type': 'text/plain'}
-    payload1 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getmempoolinfo", "params": []})
-    r1 = requests.post(connection_string, data=payload1, auth=(rpc_user, rpc_pass), headers=headers)
-    j1 = r1.json()
-    r1.close()
-
-    # Save the responses to variables
-    mempool_size = int(j1['result']['size'])
-    mempool_bytes = int(j1['result']['bytes'])
-    mempool_usage = int(j1['result']['usage'])
-    mempool_max = int(j1['result']['maxmempool'])
-
-    # Here we assemble the table
-    x = PrettyTable()
-    x.field_names = ["Name", "Value"]
-    x.align = "l"
-    x.add_rows(
-        [
-            ["Size", str(mempool_size) + " txs"],
-            ["vBytes", str(round((mempool_bytes / (1000**2)), 2)) + " MB"],
-            ["Usage", str(round((mempool_usage / (1000**2)), 2)) + " MB"],
-            ["Max", str(round((mempool_max / (1000**2)), 2)) + " MB"],
-            ["Blocks needed to clear", "~" + str(math.trunc(mempool_bytes / (1000**2))) + " blocks"]
-        ]
-    )
-    html = x.get_html_string(attributes={"class": "table is-bordered is-striped is-hoverable is-narrow"})
-    return html
-
-
 #
 # Script actually starts running here
 #
@@ -281,7 +113,98 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", table1=build_table1(), table2=build_table2(), table3=build_table3(), table4=build_table4(), version=version, currency=currency, price=price_check(currency), refresh=refresh_time(), title=page_title)
+
+    # First, check the connection
+    conn_check()
+
+    # Headers
+    headers = {'content-type': 'text/plain'}
+
+    # RPC calls to get info
+    payload1 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getnetworkinfo", "params": []})
+    r1 = requests.post(connection_string, data=payload1, auth=(rpc_user, rpc_pass), headers=headers)
+    j1 = r1.json()
+    r1.close()
+    subversion = str(j1['result']['subversion']).strip('/')
+    connections_total = int(j1['result']['connections'])
+    connections_in = int(j1['result']['connections_in'])
+    connections_out = int(j1['result']['connections_out'])
+
+    payload2 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "uptime", "params": []})
+    r2 = requests.post(connection_string, data=payload2, auth=(rpc_user, rpc_pass), headers=headers)
+    j2 = r2.json()
+    r2.close()
+    uptime = int(j2['result'])
+
+    payload3 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getmemoryinfo", "params": ["stats"]})
+    r3 = requests.post(connection_string, data=payload3, auth=(rpc_user, rpc_pass), headers=headers)
+    j3 = r3.json()
+    r3.close()
+    mem_used = int(j3['result']['locked']['used'])
+    mem_free = int(j3['result']['locked']['free'])  # noqa: F841
+    mem_total = int(j3['result']['locked']['total'])
+
+    payload4 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getblockchaininfo", "params": []})
+    r4 = requests.post(connection_string, data=payload4, auth=(rpc_user, rpc_pass), headers=headers)
+    j4 = r4.json()
+    r4.close()
+    chain = str(j4['result']['chain'])
+    blocks = int(j4['result']['blocks'])
+    initial = str(j4['result']['initialblockdownload'])
+    verificationprogress = float(j4['result']['verificationprogress'])
+    size_on_disk = float(j4['result']['size_on_disk'])
+    pruned = str(j4['result']['pruned'])
+
+    payload5 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getnetworkhashps", "params": [144]})
+    r5 = requests.post(connection_string, data=payload5, auth=(rpc_user, rpc_pass), headers=headers)
+    j5 = r5.json()
+    r5.close()
+    hash_rate_1_day = float(j5['result'])
+
+    payload6 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getnetworkhashps", "params": [-1]})
+    r6 = requests.post(connection_string, data=payload6, auth=(rpc_user, rpc_pass), headers=headers)
+    j6 = r6.json()
+    r6.close()
+    hash_rate_last_diff = float(j6['result'])
+
+    payload7 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getmininginfo", "params": []})
+    r7 = requests.post(connection_string, data=payload7, auth=(rpc_user, rpc_pass), headers=headers)
+    j7 = r7.json()
+    r7.close()
+    difficulty = float(j7['result']['difficulty'])
+
+    payload8 = json.dumps({"jsonrpc": "1.0", "id": "curltest", "method": "getmempoolinfo", "params": []})
+    r8 = requests.post(connection_string, data=payload8, auth=(rpc_user, rpc_pass), headers=headers)
+    j8 = r8.json()
+    r8.close()
+    mempool_size = int(j8['result']['size'])
+    mempool_bytes = int(j8['result']['bytes'])
+    mempool_usage = int(j8['result']['usage'])
+    mempool_max = int(j8['result']['maxmempool'])
+
+    # Values that need to be converted to look pretty on the HTML page
+    uptime = str(datetime.timedelta(seconds=uptime))
+    mem_perc = str(round(((mem_used/mem_total) * 100), 2))
+    mem_used = str(round((mem_used / 1000), 2))
+    mem_total = str(round((mem_total / 1000), 2))
+    size_on_disk = str(round((size_on_disk / (1000**3)), 2))
+    hash_rate_1_day = str(round(hash_to_hash(hash_rate_1_day), 2))
+    hash_rate_last_diff = str(round(hash_to_hash(hash_rate_last_diff), 2))
+    mempool_blocks_to_clear = str(math.trunc(mempool_bytes / (1000**2)))
+    mempool_bytes = str(round((mempool_bytes / (1000**2)), 2))
+    mempool_usage = str(round((mempool_usage / (1000**2)), 2))
+    mempool_max = str(round((mempool_max / (1000**2)), 2))
+
+    price=float(price_check(currency))
+    sats_per_currency = int(math.trunc((100000000 / price)))
+
+    return render_template("index.html",
+                           **locals(),
+                           connection_string=connection_string,
+                           currency=currency,
+                           page_title=page_title,
+                           version=version,
+                           refresh=refresh_time())
 
 
 if __name__ == "__main__":
